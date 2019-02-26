7638914e8171dcc932cde13eea595ef23f9891a9
chidambara selvan <chidambaram@infocorp.io>
2019-02-26 15:03:07 +0530

    removed all

--- src/app/shared/product.model.ts
+++
@@ -1,8 +0,0 @@
-export class Product {
-    ProductId: number;
-    Name: string;
-    Price: number;
-    Image: string;
-    Description: string;
-    DateCreated: string;
-}
--- src/app/shared/user.model.ts
+++
@@ -1,7 +0,0 @@
-export class User {
-    UserId: number;
-    Name: string;
-    Email: string;
-    Password: string;
-    UserType: string;
-}
